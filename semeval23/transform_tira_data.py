0a298299de11e36e469aa7de71c3fba11aa17eac
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-11-15 07:41:19 +0100

    mf

--- semeval23/transform_tira_data.py
+++ semeval23/transform_tira_data.py
@@ -11,14 +11,16 @@ def yield_all_data_points():
 def main(args):
     to_remove = fields_to_remove(args)
     print(f'I remove the fields {to_remove}')
-#    with open('input.jsonl') as f:
-    keys = []
-    for i in tqdm(list(yield_all_data_points())):
-        for field_to_remove in to_remove:
-            del i[field_to_remove]
-        keys += list(i.keys())
+    with open('input.jsonl', 'w') as f:
+        keys = []
+        for i in tqdm(list(yield_all_data_points())):
+            for field_to_remove in to_remove:
+                del i[field_to_remove]
+            keys += list(i.keys())
+            f.write(json.dumps(i) + '\n')
 
     print(f'The data has keys: {set(keys)}')
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='This script transforms the raw test-data to the input for algorithms')
@@ -26,6 +28,7 @@ def parse_args():
     parser.add_argument('--task', type=str, help='The task: either 1 or 2.', choices=["1", "2"], required=True)
 
     return parser.parse_args()
+
 
 def fields_to_remove(args):
     ret = ['spoiler', 'spoilerPositions']
@@ -36,6 +39,7 @@ def fields_to_remove(args):
         return set(ret)
 
     raise ValueError(f'Could not handle task {args.task}')
+
 
 if __name__ == '__main__':
     main(parse_args())
