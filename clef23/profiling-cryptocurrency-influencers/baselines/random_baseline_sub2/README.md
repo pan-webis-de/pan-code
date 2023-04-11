1cf5db68b378762b63b3ef778887be12e3b72417
Mara Chinea Rios <99406461+marachrios@users.noreply.github.com>
2023-04-11 17:08:56 +0200

    Add baselines
    docker examples

---
+++ clef23/profiling-cryptocurrency-influencers/baselines/random_baseline_sub2/README.md
@@ -0,0 +1,10 @@
+Random Baseline for subtask2 of Profiling Cryptocurrency Influencers with Few-shot Learning
+This is a baseline for influencer interest identification (subtask2).
+
+You can run it directly via: docker run pan-profiling-cryptocurrency-baselines:subtask2-random-0.0.1 --help. To use this baseline in TIRA, you can upload this image (you have to tag it accordingly, e.g., docker tag pan-profiling-cryptocurrency-baselines:subtask2-random-0.0.1  registry.webis.de/code-research/tira/<YOUR-USER-NAME>/random-baseline-subtask2:0.0.1, and push it with docker push registry.webis.de/code-research/tira/<YOUR-USER-NAME>/random-baseline-subtask2:0.0.1) and insert it with the command python3 /random-baseline-subtask-2.py --input $inputDataset/train_text.json --output $outputDir/subtask2.json (training dataset) or python3 /random-baseline-subtask-2.py --input $inputDataset/test_text.json --output $outputDir/subtask2.json (test dataset)
+
+
+Development
+You can build the Docker image via: docker build -t pan-profiling-cryptocurrency-baselines:subtask2-random-0.0.1 .
+
+To publish the image to dockerhub, run: docker push pan-profiling-cryptocurrency-baselines:subtask2-random-0.0.1
---
+++ clef23/profiling-cryptocurrency-influencers/baselines/random_baseline_sub3/random-baseline-subtask-3.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import argparse
+import os
+from pathlib import Path
+from typing import DefaultDict
+import pandas as pd
+import numpy as np
+import random
+import json
+
+random.seed(0)
+
+LABELS=['announcement', 'subjective opinion', 'financial information', 'advertising']
+
+def load_file(input_directory: Path):
+    """ Load a labels.jsonl file, convert it to array representation and return the array.
+     This function assumes that test and prediction files have the same order of works.
+    """
+    user_id=[]
+    user_text=[]
+    with open(input_directory, 'r') as inp:
+        for i in inp:
+            tmp=json.loads(i)
+            user_id.append(tmp['twitter user id'])
+            user_text.append(tmp['texts'])
+    df_to_predict = pd.DataFrame(list(zip(user_id, user_text)), columns =['twitter user id', 'text'])
+    return df_to_predict 
+
+def run_random_baseline(df_to_predict, output_file):
+    user_id=[]
+    user_probs=[]
+    user_label=[]  
+    for user, user_texts in df_to_predict.groupby("twitter user id"):
+        print (user_texts.columns)
+        user_values = [random.choice(LABELS) for i, text in enumerate(user_texts['text'])]
+        user_label.append(max(user_values))
+        user_probs.append(1.0)
+        user_id.append(user)    
+    df_output = pd.DataFrame(list(zip(user_id, user_label, user_probs)), columns =['twitter user id', 'class', 'probability'])
+    df_output.to_json(output_file, orient='records', lines=True)
+  
+    
+
+def main():
+    parser = argparse.ArgumentParser(description='This is a baseline for subtask3 influencer intent identification.')
+
+    parser.add_argument('--input', type=str, help='The input data (expected in jsonl format).', required=True)
+    parser.add_argument('--output', type=str, help='The classified output in jsonl format.', required=False)
+    args = parser.parse_args()
+    
+    df_to_predict = load_file(Path(args.input))   
+    run_random_baseline(df_to_predict, args.output)
+
+
+if __name__ == '__main__':
+    main()
+    
