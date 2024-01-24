c23a4c399a31b0398023cce32636b712c16853ec
Daniil M <tugarin62@gmail.com>
2024-01-24 12:22:05 +0000

    Added new fluency evaluator. Update backtranslation baseline. Delete data.

--- clef24/text-detoxification/baselines/backtranslation-baseline/run.sh
+++ clef24/text-detoxification/baselines/backtranslation-baseline/run.sh
@@ -1,4 +1,4 @@
 tira-run \
 	--input-dataset pan23-text-detoxification/english-tiny-20231112-training \
 	--image webis/clef24-text-detoxification-backtranslation \
-	--command '/backtranslation_baseline.py --input ${inputDataset}/input.jsonl --output ${outputDir}/references.jsonl --src_lang_id en'
+	--command '/backtranslation_baseline.py --input ${inputDataset}/input.jsonl --output ${outputDir}/references.jsonl'
