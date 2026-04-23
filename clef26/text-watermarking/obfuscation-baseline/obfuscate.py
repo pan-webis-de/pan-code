6c3d4226695e518c412218f8e2164c9716d91939
Henry010303 <henry.plutz@gmail.com>
2026-04-23 19:03:45 +0200

    Update obfuscation example

--- clef26/text-watermarking/obfuscation-baseline/obfuscate.py
+++ clef26/text-watermarking/obfuscation-baseline/obfuscate.py
@@ -21,25 +21,32 @@ def sentence_shuffle_attack(text):
 @click.argument("output_directory", type=Path)
 def main(watermarked_texts, original_texts, output_directory):
     watermarked_data = lines_if_valid(watermarked_texts, "*.jsonl")
+    watermarked_data = pd.DataFrame.from_records(watermarked_data)
 
+    truth_labels = []
     for i in range(len(watermarked_data)):
-        watermarked_data[i]["truth_label"] = "watermarked"
-
-    # attack watermark
-    for i in range(len(watermarked_data)):
-        watermarked_data[i]["text"] = sentence_shuffle_attack(watermarked_data[i]["text"])
-
-    data = {i["id"]: i for i in watermarked_data}
+        truth_labels.append("watermarked")
+    
+    watermarked_data["truth_label"] = truth_labels
 
     original_data = lines_if_valid(original_texts, "*.jsonl")
-    # we add some more original documents that are not watermarked
+    original_data = pd.DataFrame.from_records(original_data)
+    rows = []
     for i in range(len(watermarked_data)):
-        original_data[i]["truth_label"] = "not-watermarked"
-        data[original_data[i]["id"]] = original_data[i]
+        # We add the non-obfuscated watermarked text
+        rows.append({"id": watermarked_data["id"][i], "text": watermarked_data["text"][i], "truth_label": "watermarked", "type": f"unchanged"})
+        # We add the non-obfuscated original (non-watermarked) text
+        rows.append({"id": original_data["id"][i], "text": original_data["text"][i], "truth_label": "not-watermarked", "type": f"unchanged"})
+        # We attack the watermarked text
+        rows.append({"id": watermarked_data["id"][i], "text": sentence_shuffle_attack(watermarked_data["text"][i]), "truth_label": "watermarked", "type": f"sentence-shuffle"})
+        # We attack the original non-watermarked text
+        rows.append({"id": watermarked_data["id"][i], "text": sentence_shuffle_attack(original_data["text"][i]), "truth_label": "not-watermarked", "type": f"sentence-shuffle"})
+
+    data = pd.DataFrame(rows)
 
     # attention: the field truth_label is removed from the tira workflow for system inputs, but not for the truth data
     output_directory.mkdir(exist_ok=True, parents=True)
-    pd.DataFrame(data.values()).to_json(output_directory / "texts.jsonl", lines=True, orient="records")
+    data.to_json(output_directory / "texts.jsonl", lines=True, orient="records")
 
 
 if __name__ == '__main__':
